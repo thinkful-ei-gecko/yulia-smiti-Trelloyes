69b8e7cdacb61717b5fb773b99496eed2173fb15
SmitiShakya <smitishk12@gmail.com>
2019-09-03 11:32:33 -0500

    card.js added

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+function Card(props){
+    console.log(props)
+    return (
+        <div class="Card">
+            <button type="button">delete</button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    );
+};
+
+export default Card;
